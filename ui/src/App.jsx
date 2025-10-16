e7abd846cde68210b3e1dd2bbc020b23f3701d6e
CristopherPaiz <cpaizl1@miumg.edu.gt>
2025-10-15 18:21:02 -0600

    UI inicial desde plantilla

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,1 +1,7 @@
- 
+import React from "react";
+
+const App = () => {
+  return <div className="text-2xl font-bold">Hello World!</div>;
+};
+
+export default App;
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,23 +1,14 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import tailwind from "@tailwindcss/vite";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwind()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true,
     },
   },
 });
